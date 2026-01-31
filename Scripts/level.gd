efe976202c10f8e4d3d845427eba809553bc597d
TaliaKah <anaissaratagougne@tutanota.com>
2026-01-31 03:31:39 +0100

    [level] Add severals obstacles

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,6 +1,6 @@
 extends Node3D
-var obstacle_scene: PackedScene = preload("res://Scenes/obstacle.tscn")
 
+@export var obstacle_scenes:Array[PackedScene]
 
 @export var x_spawn : float = 50.
 @export var y_spawn : float = 10.
@@ -11,7 +11,7 @@ var obstacle_scene: PackedScene = preload("res://Scenes/obstacle.tscn")
 var obstacle_count : int = 0
 
 func spawn_obstacle()-> void:
-	var obstacle_to_spawn = obstacle_scene.instantiate()
+	var obstacle_to_spawn = (obstacle_scenes.pick_random()).instantiate()
 	add_child(obstacle_to_spawn)
 	obstacle_to_spawn.set_global_position(Vector3(x_spawn,0. , randf_range(-y_spawn, y_spawn)))
 	obstacle_count+=1
