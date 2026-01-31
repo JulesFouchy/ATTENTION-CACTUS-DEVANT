1354caeac83598cb72f7f3f960c525d9fe7ef1f6
TaliaKah <anaissaratagougne@tutanota.com>
2026-01-31 15:26:16 +0100

    [level] Spawn masks and obstacles scenes in same file

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,39 +2,43 @@ extends Node3D
 @onready var ui_game_over: Control = %UiGameOver
 
 @export var obstacle_scenes:Array[PackedScene]
+@export var mask_scenes:Array[PackedScene]
 
 @export var x_spawn : float = 50.
 @export var y_spawn : float = 10.
 @export var TimeInSeconds : float = 1.
-@export var max_obstacles : int = 20
+@export var max_movables : int = 20
 
-var obstacle_count : int = 0
+var movables_count : int = 0
 
-func spawn_obstacle()-> void:
-	var obstacle_to_spawn = (obstacle_scenes.pick_random()).instantiate()
-	if obstacle_count < max_obstacles && randf() < obstacle_to_spawn.spawn_probability :
-		add_child(obstacle_to_spawn)
-		obstacle_to_spawn.set_global_position(Vector3(x_spawn,0. , randf_range(-y_spawn, y_spawn)))
-		obstacle_count+=1
-	
 func _ready() -> void:
 	$Timer.start(TimeInSeconds)
 	State.YouLost.connect(_on_you_lost)
 	State.TryAgain.connect(_on_try_again)
-	State.destroyed_obstacle.connect(_on_obstacle_destroyed)
+	State.destroyed_obstacle.connect(_on_movable_destroyed)
 	
+func spawn(scene_to_spawn) -> void:
+	add_child(scene_to_spawn)
+	scene_to_spawn.set_global_position(Vector3(x_spawn,0. , randf_range(-y_spawn, y_spawn)))
+	movables_count+=1
+	
+func _on_timer_timeout():
+	var scene_to_spawn = (obstacle_scenes.pick_random()).instantiate()
+	var obstacle_to_spawn = (obstacle_scenes.pick_random()).instantiate()
+	var mask_scene = (mask_scenes.pick_random()).instantiate()
+	if movables_count < max_movables && randf() < obstacle_to_spawn.spawn_probability :
+		spawn(obstacle_to_spawn)
+		spawn(mask_scene)
+
 func _input(event):
 	if Input.is_action_just_pressed("interact"):
 		State.MaskChanged.emit()
+	
+func _on_movable_destroyed():
+	movables_count -= 1
 	
 func _on_you_lost() -> void:
 	ui_game_over.visible = true
 	
 func _on_try_again() -> void:
 	ui_game_over.visible = false
-
-func _on_timer_timeout():
-	spawn_obstacle()
-
-func _on_obstacle_destroyed():
-	obstacle_count -= 1
