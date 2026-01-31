27a4b8c8eac38b8f0460e33eac2e97257df2d126
TaliaKah <anaissaratagougne@tutanota.com>
2026-01-31 03:04:35 +0100

    [level] rewrite

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,11 +1,28 @@
 extends Node3D
 var obstacle_scene: PackedScene = preload("res://Scenes/obstacle.tscn")
 
+
 @export var x_spawn : float = 50.
 @export var y_spawn : float = 10.
 @export var TimeInSeconds : float = 1.
+@export var max_obstacles : int = 20
+
+var obstacle_count : int = 0
 
 func spawn_obstacle()-> void:
 	var obstacle_to_spawn = obstacle_scene.instantiate()
 	add_child(obstacle_to_spawn)
 	obstacle_to_spawn.set_global_position(Vector3(x_spawn,0. , randf_range(-y_spawn, y_spawn)))
+	obstacle_count+=1
+	
+func _ready() -> void:
+	$Timer.start(TimeInSeconds)
+
+func _on_timer_timeout():
+	if obstacle_count < max_obstacles :
+		spawn_obstacle()
+		print("create",obstacle_count)
+
+func _on_obstacle_destroyed():
+	obstacle_count -= 1
+	print("delete",obstacle_count)
