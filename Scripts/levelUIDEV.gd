61122fa4578af70b83cfd6b23cba1b0661e41e38
sophielagier <sophielagier.contact@gmail.copm>
2026-01-31 04:37:37 +0100

    update ux
    les objects popent sur la route

--- Scripts/levelUIDEV.gd
+++ Scripts/levelUIDEV.gd
@@ -1,29 +1,36 @@
 extends Node3D
-var morceaumask: PackedScene = preload("res://Scenes/morceau_de_masque3D.tscn")
 
+@export var masques_scenes:Array[PackedScene]
 
-@export var x_spawn : float = 50.
+@export var x_spawn : float = 10.
 @export var y_spawn : float = 10.
-@export var TimeInSeconds : float = 1.
+@export var TimeInSeconds : float = .5
 @export var max_obstacles : int = 20
 
-var obstacle_count : int = 15
+var obstacle_count : int = 0
 
 func spawn_obstacle()-> void:
-	var obstacle_to_spawn = morceaumask.instantiate()
-	add_child(obstacle_to_spawn)
-	obstacle_to_spawn.set_global_position(Vector3(x_spawn,0. , randf_range(-y_spawn, y_spawn)))
+	print("rrrr")
+	var masques_scene = (masques_scenes.pick_random()).instantiate()
+
+	add_child(masques_scene)
+	print("nouveau")
+	masques_scene.set_global_position(Vector3( x_spawn , randf_range(-y_spawn, y_spawn),0))
 	obstacle_count+=1
+		
 	
-	obstacle_to_spawn.connect("destroyed_obstacle", Callable(self, "_on_obstacle_destroyed"))
 	
 func _ready() -> void:
+	print("Timer exists:", $Timer)
+	print("Timer wait time:", $Timer.wait_time)
+	print("Timer one_shot:", $Timer.one_shot)
+
 
 	$Timer.start(TimeInSeconds)
 
 func _on_timer_timeout():
-	if obstacle_count < max_obstacles :
-		spawn_obstacle()
+	print("timeout")
+	spawn_obstacle()
 
 func _on_obstacle_destroyed():
 	obstacle_count -= 1
