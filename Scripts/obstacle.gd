efce9c0d7cc87bbc50368149665d6e3ee2e1b497
TaliaKah <anaissaratagougne@tutanota.com>
2026-01-31 22:38:23 +0100

    Repair rebase v2

--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -12,10 +12,5 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("player"):
 		get_node("/root/Scripts/scene_fader").start_fade()  # Appelle le fondu
 
-
 func _apply_mask() -> void:
 	pass
-
-func destroy():
-	State.destroyed_obstacle.emit()
-	super.destroy()
