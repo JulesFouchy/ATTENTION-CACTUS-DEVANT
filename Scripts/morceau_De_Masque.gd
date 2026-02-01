63bfd32fb806f8af7fb1bc3a64f5b716c4edfd31
Jules Fouchy <jules.fouchy@ntymail.com>
2026-02-01 11:12:24 +0100

    Fix mouse 3D picking

--- Scripts/morceau_De_Masque.gd
+++ Scripts/morceau_De_Masque.gd
@@ -4,13 +4,11 @@ class_name MorceauMasque3D
 @export var iconemasque_scene: PackedScene
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			var gui_instance = iconemasque_scene.instantiate()
 			gui_instance.texture = get_child(1).texture
 			gui_instance.get_child(0).queue_free()
-			print(gui_instance.texture)
 			gui_instance.position = get_viewport().get_mouse_position()
 			gui_instance.position.x -=40
 			gui_instance.position.y -=40
